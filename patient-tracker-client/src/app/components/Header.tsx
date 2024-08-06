9258b2b4b095b61b3fc0a4656d657849356ebb3a
uladaharanina <uladaharanina@gmail.com>
2024-08-05 22:29:05 -0400

    add useEffect

--- patient-tracker-client/src/app/components/Header.tsx
+++ patient-tracker-client/src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { useState, useContext } from "react";
-import { UserContext } from "../UserContext";
+import { useState, useContext, useEffect } from "react";
+import  UserContext  from "../UserContext";
 import { UserData } from "./UserData"
 import HeaderStyle from '../styles/HeaderStyle.module.css';
 
@@ -10,29 +10,36 @@ interface HeaderProps{
 
 export const Header:React.FC<HeaderProps> = (props) => {
 
-    const [isUserLogged, setUserLoginStatus] = useState(true);
+    const { username, setUsername } = useContext(UserContext);
+    const [isUserLogged, setUserLoginStatus] = useState(username != "User");
 
-    const handleUserLoging = () =>{
-
-        if(isUserLogged){
-            setUserLoginStatus(false);
-            props.onLogout();
-            
-        }
+     // useEffect updates isUserLogged whenever username changes. 
+     //This ensures that the state reflects the current value of username correctly.
+     useEffect(() => {
+        setUserLoginStatus(username !== "User");
+    }, [username]); // The dependency array [username] ensures that the effect runs only when username changes.
+    
+    const onLogout = ()  => {
+        setUsername("User");
+        setUserLoginStatus(false);
     }
-
     return(
         <header id={HeaderStyle.header}>
             <UserData></UserData>
             <nav>
                 <ul>
+                    {isUserLogged}
                     <li className={HeaderStyle.li}>Home</li>
                     <li className={HeaderStyle.li}>About</li>
                     <li className={HeaderStyle.li}>Contacts</li>
                 </ul>
             </nav>
             
-            <button onClick={handleUserLoging}>{isUserLogged ? "Logout": "Login"}</button>
+             {/* Conditional rendering */}
+             {isUserLogged && (
+                <button onClick={onLogout}>Logout</button>
+            )}
+            
         </header>
     )
 }
